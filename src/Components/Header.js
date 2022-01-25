bc0e92048bf3f1189149cb60b5b3e9b23dfbfe7c
Karen <karen.wen3@gmail.com>
2022-01-25 13:00:17 -0500

    header style changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,19 @@
 import logo from './images/logo.png'
-import { BsBag } from 'react-icons/bs';
+import { BsBag, BsSuitHeart, BsPerson } from 'react-icons/bs';
 import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <div className="header">
-            <div>SHOP</div>
+            <div className="header-left">
+                <Link to='/shopall'><div>SHOP</div></Link>
+            </div>
             <Link to='/'><img src={logo} className="logo" alt="logo" /></Link>
-            <div><BsBag /></div>
+            <div className="header-right">
+                <Link to='/login'><div><BsPerson /></div></Link>
+                <Link to='/wishlist'><div><BsSuitHeart /></div></Link>
+                <Link to='/cart'><div><BsBag /></div></Link>
+            </div>
         </div>
     );
 }
