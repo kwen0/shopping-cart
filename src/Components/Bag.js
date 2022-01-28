82fd279074552bb662a8b107d56422583cea456c
Karen <karen.wen3@gmail.com>
2022-01-28 12:50:36 -0500

    finished checkout summary box

--- src/Components/Bag.js
+++ src/Components/Bag.js
@@ -10,7 +10,7 @@ function Bag(props) {
         <div>
             <Header bag={bag} bagCount={bagCount} />
             <div className="bag summary">
-                {bag.length !== 0 ? bag.map(item => (
+                <div>{bag.length !== 0 ? bag.map(item => (
                     <div className="bag row" key={item.id}>
                         <img src={item.image} alt="jewelry" className="bag img" />
                         <div className="bag details">
@@ -23,21 +23,33 @@ function Bag(props) {
                             </div>
                             <div class="edit">
                                 <div class="edit-qty">
-                                    <button class="btn" onClick={() => removeFromBag(item)}>-</button>
-                                    <div class="qty">{item.qty}</div>
-                                    <button class="btn" onClick={() => addToBag(item)}>+</button>
+                                    <button className="btn" onClick={() => removeFromBag(item)}>-</button>
+                                    <div className="qty">{item.qty}</div>
+                                    <button className="btn" onClick={() => addToBag(item)}>+</button>
                                 </div>
                                 <div className="prod price">${item.price * item.qty}</div>
                             </div>
                         </div>
                     </div>
-                )) : <div>Your bag is empty!</div>}
-                <div className="checkout">
-                    <div className="wrapper">
-                        <div>Subtotal: {total}</div>
-                        <div>Shipping: free</div>
-                        <div>{quantity}</div>
-                    </div>
+                )) : <div>Your bag is empty!</div>}</div>
+                <div>
+                    {bag.length !== 0 ? <div className="checkout summary">
+                        <div className="checkout row">
+                            <div>Subtotal:</div>
+                            <div>${total} USD</div>
+                        </div>
+                        <div className="checkout row">
+                            <div>Shipping:</div>
+                            <div>Free</div>
+                        </div>
+                        <div className="checkout row total">
+                            <div>Estimated Total:</div>
+                            <div>${total} USD</div>
+                        </div>
+                        <div className="checkout row">
+                            <button className="checkout btn">CONTINUE TO CHECKOUT</button>
+                        </div>
+                    </div> : null}
                 </div>
             </div >
             <Footer />
