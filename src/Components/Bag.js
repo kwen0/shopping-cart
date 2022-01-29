7be8b2a2085ebd00e57db2124232768ff557a7a2
Karen <karen.wen3@gmail.com>
2022-01-29 13:57:14 -0500

    updated styles for mobile

--- src/Components/Bag.js
+++ src/Components/Bag.js
@@ -1,6 +1,5 @@
 import Header from './Header';
 import Footer from './Footer';
-import { AiOutlineClose } from 'react-icons/ai'
 import { Link } from 'react-router-dom';
 import BagItem from './BagItem';
 
